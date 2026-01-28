2c931cc51c1eccf0f7b0b3e2ee78e8c99de954df
JVancata <vancata.kuba@seznam.cz>
2026-01-28 09:59:43 +0100

    semicolons

--- apps/chat-api/index.ts
+++ apps/chat-api/index.ts
@@ -1,8 +1,8 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(cors())
 app.use(express.json());
